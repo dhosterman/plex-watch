2eda7bfe575f2f93e5e816f1b91842abcbdf8985
Daniel Hosterman <daniel.hosterman@gmail.com>
2015-06-21 14:22:08 -0400

    Formatting.

--- src/plex_watch/core.clj
+++ src/plex_watch/core.clj
@@ -8,16 +8,15 @@
   (some? (re-matches #".*\.mkv|.*\.mp4|.*\.avi" filename)))
 
 (defn send_push [filename]
-  (client/post 
-    "https://api.pushbullet.com/v2/pushes"
-    {:headers {:Authorization (str "Bearer " (config :token))}
-     :content-type :json
-     :form-params {:type "note"
-                   :title "New video available"
-                   :body filename}}))
+  (client/post "https://api.pushbullet.com/v2/pushes"
+               {:headers {:Authorization (str "Bearer " (config :token))}
+                :content-type :json
+                :form-params {:type "note"
+                              :title (or (config :title) "New video available")
+                              :body filename}}))
 
 (defn watch_handler [ctx e]
-  (let [filename (.getName(:file e))] 
+  (let [filename (.getName (:file e))] 
     (if (is_video? filename)
       (send_push filename)))
   ctx)
--- test/plex_watch/core_test.clj
+++ test/plex_watch/core_test.clj
@@ -5,5 +5,5 @@
 (deftest is_video?_test
   (testing "test.avi is a video"
     (is (= (is_video? "test.avi") true))
-  (testing "test.txt is not a video"
-    (is (= (is_video? "test.txt") false)))))
+    (testing "test.txt is not a video"
+      (is (= (is_video? "test.txt") false)))))
